c9a8e2531ca17a3e88553a8492849f5d725059a3
Thiago Elias Chianca <c148738@corp.caixa.gov.br>
2024-03-18 16:53:30 -0300

    Testando envio pela IDE

---
+++ src/br/gov/caixa/NotaFiscal.java
@@ -0,0 +1,89 @@
+package br.gov.caixa;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class NotaFiscal {
+    int numeroNotaFiscal;
+    String descricaoProduto;
+    int quantidadeComprada;
+    double precoProduto;
+
+    public NotaFiscal(int numeroNotaFiscal, String descricaoProduto, int quantidadeComprada, double precoProduto) {
+        this.numeroNotaFiscal = numeroNotaFiscal;
+        this.descricaoProduto = descricaoProduto;
+        this.quantidadeComprada = quantidadeComprada;
+        this.precoProduto = precoProduto;
+    }
+
+    public String nf_to_String() {
+        String saida;
+        NumberFormat formatter = new DecimalFormat("#0.00");
+
+        saida = this.numeroNotaFiscal + "|" + this.descricaoProduto + "|" + this.quantidadeComprada + "|" + formatter.format(this.precoProduto);
+        return saida;
+    }
+}
+
+class NotaFiscalEletronica extends NotaFiscal {
+    String qrCode;
+
+    public NotaFiscalEletronica(int numeroNotaFiscal, String descricaoProduto, int quantidadeComprada, double precoProduto, String qrCode) {
+        super(numeroNotaFiscal, descricaoProduto, quantidadeComprada, precoProduto);
+        this.qrCode = qrCode;
+    }
+
+
+    public String nf_to_String() {
+        String saida;
+        NumberFormat formatter = new DecimalFormat("#0.00");
+
+        saida = this.numeroNotaFiscal + "|" + this.descricaoProduto + "|" + this.quantidadeComprada + "|" + formatter.format(this.precoProduto) + "|" + this.qrCode;
+        return saida;
+    }
+}
+
+class Solution {
+    public static String solution(String entrada) {
+        //"nf:1_desc:Camiseta_qtde:1_precoUnitario:49,90_qrCode:abc"
+        String[] entradaSemUnderScore = entrada.split("_");
+        int numeroNotaFiscal = 0;
+        String descricaoProduto = "";
+        int quantidadeComprada = 0;
+        double precoProduto = 0.00;
+        String qrCode = "";
+        for (int i = 0; i < entradaSemUnderScore.length; i++) {
+            switch (entradaSemUnderScore[i].substring(0, entradaSemUnderScore[i].indexOf(":"))) {
+                case "nf":
+                    numeroNotaFiscal = Integer.parseInt(entradaSemUnderScore[i].substring(entradaSemUnderScore[i].indexOf(":") + 1));
+                case "desc":
+                    descricaoProduto = entradaSemUnderScore[i].substring(5);
+                case "qtde":
+                    quantidadeComprada = Integer.parseInt(entradaSemUnderScore[i].substring(5));
+                case "precoUnitario":
+                    precoProduto = Double.parseDouble(entradaSemUnderScore[i].substring(14).replace(',', '.'));
+                case "qrCode":
+                    qrCode = entradaSemUnderScore[i].substring(7);
+            }
+
+
+        }
+            NotaFiscalEletronica nfe = new NotaFiscalEletronica(numeroNotaFiscal, descricaoProduto, quantidadeComprada, precoProduto, qrCode);
+            return nfe.nf_to_String();
+
+        }
+    }
+
+
+
+class TesteNotas {
+    public static void main(String[] args) {
+        String entrada = "nf:1_desc:Camiseta_qtde:1_precoUnitario:49,90_qrCode:abc";
+        //String entrada2 = "nf:1_desc:Camiseta_qtde:1_precoUnitario:49,90";
+
+        System.out.println(Solution.solution(entrada));
+
+
+
+    }
+}
--- src/br/gov/caixa/Veiculo.java
+++ src/br/gov/caixa/Veiculo.java
@@ -1,24 +1,86 @@
 package br.gov.caixa;
 
-public abstract class Veiculo {
-    protected String cor;
-    protected  String tamanho;
-    protected String marca;
-    protected int ano;
-    protected int quantidadePneus;
-    protected String nome;
-    protected int velocidade;
+import java.util.ArrayList;
 
-    public void andar() {
-        
+class Veiculo {
+    public String placa;
+    public String tipo;
+
+    public Veiculo(String placa) {
+        this.placa = placa;
     }
-    public void acelerar() {
+}
 
+// Criar classe carro
+class Carro extends Veiculo {
+
+    // Construtor recebendo tipo
+    public Carro(String placa) {
+        super(placa);
+        this.tipo = "carro";
     }
-    public void freiar() {
+}
 
+//Criar classe moto
+class Moto extends Veiculo {
+
+    // Construtor recebendo tipo
+    public Moto(String placa) {
+        super(placa);
+        this.tipo = "moto";
+    }
+}
+
+class Estacionamento {
+    //ArrayList de veiculos
+    ArrayList<Veiculo> veiculosEstacionados = new ArrayList<Veiculo>();
+
+    //Metodo Estacionar
+    public void estacionar(Veiculo v) {
+        veiculosEstacionados.add(v);
+    }
+
+    //Metodo Sair do Estacionamento (Placa)
+    public Veiculo sairDoEstacionamento(String placa) {
+        for (Veiculo veiculo : veiculosEstacionados) {
+            if(veiculo.placa.equals(placa)) {
+                return veiculo;
+            }
+        }
+        return null;
+    }
+
+    //Metodo de consultar
+    public boolean consultarVeiculo(String placa){
+        for (Veiculo veiculo : veiculosEstacionados) {
+            if(veiculo.placa.equals(placa)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Metodo da ocupação
+    public int ocupacao() {
+        int valor = veiculosEstacionados.size();
+        return valor;
     }
 
 
 
+
+}
+
+class Teste {
+    public static void main(String[] args) {
+        Carro c = new Carro("123");
+        Estacionamento e = new Estacionamento();
+        System.out.println("Estacionando 123");
+        e.estacionar(c);
+        System.out.println(e.ocupacao());
+        System.out.println("Retirando 123");
+        Veiculo v = e.sairDoEstacionamento("123");
+
+        System.out.println(e.ocupacao());
+    }
 }
